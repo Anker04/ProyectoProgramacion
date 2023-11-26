ca00827aec37a5c40d66a81bb8e838bfb5663a58
Nelson Quiñinao <127786657+Anker04@users.noreply.github.com>
2023-11-26 12:15:54 -0400

    Update Proyecto.py
    plantas

--- Proyecto.py
+++ Proyecto.py
@@ -15,6 +15,10 @@ sand_texture = arcade.load_texture("arena.png")
 cebra_texture = arcade.load_texture("vaca.png")
 leon_texture = arcade.load_texture("lobo.png")
 
+# plantas
+tree_texture = arcade.load_texture("arbol.png")
+Planta_texture = arcade.load_texture("planta.png")
+
 # Crear la matriz del mapa
 mapa = [
     [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
@@ -25,17 +29,17 @@ mapa = [
     [1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
     [1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
     [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
-    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
+    [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1],
 ]
 
 
@@ -63,6 +67,7 @@ class Animal(Organismo):
         self.especie = especie
         self.dieta = dieta
         self.ciclos = 0
+        self.velocidad = 10  # Ajusta la velocidad según tus preferencias
 
     def cazar(self, presa):
         # Lógica de caza
@@ -89,12 +94,13 @@ class Animal(Organismo):
 
 
 class Planta(Organismo):
-    def __init__(self, x, y):
+    def __init__(self, x, y, especie="planta"):
         super().__init__(x, y)
+        self.especie = especie
 
     def fotosintesis(self):
         # Lógica de fotosíntesis
-        pass
+        self.energia += 5
 
 
 class Ambiente:
@@ -127,8 +133,12 @@ class Ecosistema:
         # Lógica para inicializar el ecosistema
         animal1 = Animal(5, 5, "lobo", "carnivoro")
         animal2 = Animal(8, 8, "vaca", "Herbívoro")
+        planta1 = Planta(3, 3, "planta")
+        planta2 = Planta(12, 12, "arbol")
         self.matriz_espacial[animal1.x][animal1.y] = animal1
         self.matriz_espacial[animal2.x][animal2.y] = animal2
+        self.matriz_espacial[planta1.x][planta1.y] = planta1
+        self.matriz_espacial[planta2.x][planta2.y] = planta2
 
         # Lógica para inicializar el ecosistema
 
@@ -138,6 +148,8 @@ class Ecosistema:
                 organismo = self.matriz_espacial[fila][columna]
                 if isinstance(organismo, Animal):
                     organismo.mover_aleatoriamente(self)
+                elif isinstance(organismo, Planta):
+                    organismo.fotosintesis()
 
     def cadena_alimenticia(self):
         # Lógica de la cadena alimenticia
@@ -184,13 +196,26 @@ class EcosistemaVisual(arcade.Window):
                     texture,
                     0,
                 )
-                # Dibujar textura del animal si hay uno en esa posición
+                # Dibujar textura del animal o planta si hay uno en esa posición
                 organismo = self.ecosistema.matriz_espacial[fila][columna]
                 if isinstance(organismo, Animal):
                     if organismo.especie == "lobo":
-                        texture = leon_texture  # Corregir a leon_texture
+                        texture_animal = leon_texture
                     elif organismo.especie == "vaca":
-                        texture = cebra_texture  # Corregir a vaca_texture
+                        texture_animal = cebra_texture
+                    arcade.draw_texture_rectangle(
+                        x + tile_size / 2,
+                        y + tile_size / 2,
+                        tile_size,
+                        tile_size,
+                        texture_animal,
+                        0,
+                    )
+                elif isinstance(organismo, Planta):
+                    if organismo.especie == "arbol":
+                        texture = tree_texture
+                    else:
+                        texture = Planta_texture
                     arcade.draw_texture_rectangle(
                         x + tile_size / 2,
                         y + tile_size / 2,
